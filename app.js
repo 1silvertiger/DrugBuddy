6544d47d455fedf0a2d2b7c32f60f8fa355b15fc
Dakota Dalton <1silvertiger@gmail.com>
2019-07-16 21:43:38 -0500

    Added Vue and Materialize

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const express = require('express');
 
+const app = express();
+app.listen(8080);
+
+console.log('Express listening on port 8080');
+
+app.use(express.static('./dist'));
+
+// Static references
+app.use('/css', express.static(__dirname + '/node_modules/materialize-css/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/materialize-css/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/vue/dist'))
+
+// app.all('/', (req, res) => {
+//     res.send();
+// });
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+   const app = new Vue({
+      el: '#app',
+      data: {
+
+      },
+      mounted: function () { },
+      methods: {
+
+      }
+   });
+});
