dab555f3d5b476a1bce3f338902021dfec2bd1b9
Dakota Dalton <1silvertiger@gmail.com>
2019-12-12 18:41:28 -0600

    First call successfull
    Can show data in a table

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
    const app = new Vue({
       el: '#app',
       data: {
-
+         interactions: new Array(),
+         uri: "https://rxnav.nlm.nih.gov/REST/interaction/interaction.json"
       },
-      mounted: function () { },
+      mounted: function () {
+         let $vm = this;
+         fetch(`${$vm.uri}?rxcui=88014`)
+         .then(response => { return response.json(); })
+         .then(returnObj => {
+            $vm.interactions = returnObj.interactionTypeGroup[0].interactionType[0].interactionPair;
+            //console.log(returnObj);
+         });
+      },
       methods: {
 
       }
